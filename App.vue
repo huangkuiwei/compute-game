f2ddd87566bc5110dc706684221521b4980a3471
huangkuiwei <1017969390@qq.com>
2024-11-24 20:33:21 +0800

    提交

---
+++ App.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  onLaunch: function() {
+    let moneyData = uni.getStorageSync('moneyData')
+
+    if (!moneyData) {
+      uni.setStorageSync('moneyData', [
+        {
+          minMoney: 0.9,
+          maxMoney: 1.5
+        },
+      ])
+    }
+  },
+}
+</script>
+
+<style>
+/*每个页面公共css */
+@import '/common/styles/reset.scss';
+@import '/common/styles/global.scss';
+</style>
---
+++ pages/math/math.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'math'
+}
+</script>
+
+<template>
+  <view class="math-page">math-page</view>
+</template>
+
+<style lang="scss">
+
+</style>
